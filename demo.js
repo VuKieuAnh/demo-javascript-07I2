5653774209a30724f9bff71cfc6d5e17746f5050
VuKieuAnh <vukieuanh.hnue@gmail.com>
2020-08-08 10:36:38 +0700

    add javascript

---
+++ demo.js
@@ -0,0 +1,10 @@
+// alert("Xin chao C07I2");
+let ten = prompt("Moi ban nhap ten");
+// alert(ten);
+let gioitinh = confirm("Ban co phai la nam khong?");
+// alert(gioitinh);
+// document.write("Giới tính nam? " + gioitinh);
+document.getElementById("name").value = ten;
+alert("gioi tinh" + gioitinh)
+// document.getElementById("gioitinh").innerText = gioitinh;
+console.log(ten + " " + gioitinh);
